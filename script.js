3e1bdb86ff7a6133fa176dbed5fc5cc95e85ddf9
Kaif-ali-khan <154369130+Kaifali852@users.noreply.github.com>
2024-11-19 16:15:10 +0500

    calculator completed

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-const inputField = document.getElementById("screen");
-const btns = document.querySelectorAll("button");
-  
-inputField.addEventListener("change", (event) => {
-  let input = event.target.value;
-  console.log(input);
-});
+let string = "";
+let buttons = document.querySelectorAll(".button");
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    console.log(e.target.value);
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    console.log("click");
+    if (e.target.innerHTML === "=") {
+      string = eval(string);
+      document.querySelector("#screen").value = string;
+    } else if (e.target.innerHTML === "AC") {
+      string = "";
+      document.querySelector("#screen").value = string;
+    } else if (e.target.innerHTML === "Del") {
+      string = string.slice(0, -1);
+      document.querySelector("#screen").value = string;
+    } else {
+      string = string + e.target.innerHTML;
+      document.querySelector("#screen").value = string;
+    }
   });
 });
